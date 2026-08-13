8faa39ecfe258d5d70654456895f844ac5bbc055
srijanadhikary <srijanadhikary@gmail.com>
2026-08-13 06:02:18 +0545

    Change themeColor to a new hex value

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   colorScheme: 'light',
-  themeColor: '#22366b',
+  themeColor: '#043ABA',
 }
 
 export default function RootLayout({
